59a79c2ad7858df7e08eb78388f9ba23eeafbaa8
the-darknes <magist.dev@hotmail.com>
2025-06-24 00:08:16 +0300

    feat: add maintenance redirect in AuthLayout component
    - Implemented a useEffect hook to check for maintenance mode using an environment variable.
    - Redirects users to the maintenance page if the application is in maintenance mode.

--- src/app/auth/AuthLayout.tsx
+++ src/app/auth/AuthLayout.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import type { Metadata } from 'next'
 import './.././globals.css'
 import { Toaster } from 'sonner'
+import { useEffect } from 'react'
 
 export const metadata: Metadata = {
 	title: 'Auth - Hasher',
@@ -12,6 +15,12 @@ export default function AuthLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
+	useEffect(() => {
+		const maintenance = process.env.NEXT_PUBLIC_ON_MAINTENANCE
+		if (maintenance === undefined || maintenance === 'true') {
+			window.location.href = '/maintenance/'
+		}
+	})
 	return (
 		<div
 			className={`flex h-full w-full flex-col items-center justify-center antialiased`}
